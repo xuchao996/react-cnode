6fd486b431dff306d1e28871fcd4c6b1aa032188
cao_zhaokun <359318202@qq.com>
2019-09-09 23:00:27 +0800

    引入sass做预编译

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import "./App.css"
 import "./reset.css"
-import {Component} from "react"
+import "./App.scss"
+// import {Component} from "react"
 
+import { Link } from "react-router";
 import logo from "./asserts/cnodejs_light.svg"
 
 // class App extends Component () {
@@ -45,12 +46,12 @@ function App (props) {
             <input />
           </div>
           <ul className="header-tab">
-            <li>首页</li>
-            <li>新手入门</li>
-            <li>API</li>
-            <li>关于</li>
-            <li>注册</li>
-            <li>登陆</li>
+            <li><Link to="home">首页</Link></li>
+            <li><Link to="getstart">新手入门</Link></li>
+            <li><Link to="api">API</Link></li>
+            <li><Link to="about">关于</Link></li>
+            <li><Link to="register">注册</Link></li>
+            <li><Link to="login">登陆</Link></li>
           </ul>
         </header>
         {props.children}
--- src/routers/router.js
+++ src/routers/router.js
@@ -14,6 +14,10 @@ const router = [
 		indexRoute: {component: Home},
 		childRoutes: [
 			{
+				path: 'home',
+				component: Home
+			},
+			{
 				path: 'getstart',
 				component: Getstart
 			},
