146570e9cbd5e043441c17e4772cc8dffc941851
eric <xuchao@gisiinfo.com>
2020-01-13 12:50:29 +0800

    up

--- src/views/userDetail.js
+++ src/views/userDetail.js
@@ -1,23 +1,55 @@
-import React, {Component} from "react"
+import React, { Component } from "react"
 
 import { getUserDetail } from "../Api";
 
-import {ContentContainer, AsideContainer} from "../components";
+import { ContentContainer, AsideContainer } from "../components";
 
-class TopicDetail extends Component{
-    constructor (props) {
+import "./userDetail.scss"
+
+function List(props, ItemFn) {
+    const { list } = props;
+    return (
+        <ul className="recent-reply-list">
+            {list.map(item => {
+                return ItemFn(item)
+            })}
+        </ul>
+    )
+}
+
+function ReplyItem(props) {
+    return (
+        <li></li>
+    )
+}
+function TopicItem(props) {
+    return (
+        <li></li>
+    )
+}
+
+function RecentReply(props) {
+    return List(props, ReplyItem)
+}
+
+function RecentTopics(props) {
+    return List(props, TopicItem)
+}
+
+
+class TopicDetail extends Component {
+    constructor(props) {
         super(props)
         this.state = {
             detail: {}
         }
     }
-    componentDidMount () {
+    componentDidMount() {
         const id = this.props.params.id;
         this.getData(id)
     }
-    getData (id) {
+    getData(id) {
         getUserDetail(id).then((response) => {
-            console.log(response)
             if (response.success) {
                 this.setState({
                     detail: response.data
@@ -25,21 +57,26 @@ class TopicDetail extends Component{
             }
         })
     }
-	render() {
-        const {loginname} = this.state.detail;
-		return (
-			<div className="home user">
-				<ContentContainer>
+    render() {
+        const { loginname, avatar_url, recent_replies = [], recent_topics, score } = this.state.detail;
+
+        return (
+            <div className="home user">
+                <ContentContainer>
                     <div className="user-info">
+                        <img src={avatar_url} alt="" />
                         <p>{loginname}</p>
+                        <p>{score}</p>
                     </div>
+                    <RecentReply list={recent_replies}></RecentReply>
                     {/* 最近创建的话题列表 */}
+                    <RecentTopics list={recent_topics}></RecentTopics>
                     {/* 最近参与的话题列表 */}
                 </ContentContainer>
                 <AsideContainer></AsideContainer>
-			</div>
-		);
-	}
+            </div>
+        );
+    }
 
 };
 
